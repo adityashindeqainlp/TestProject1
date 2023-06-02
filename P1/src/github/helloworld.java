7d24798e4230c259e39aa97ea0197638d2adafe5
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-06-02 13:58:16 -0400

    Commit001

---
+++ P1/src/github/helloworld.java
@@ -0,0 +1,16 @@
+package github;
+
+/**
+ * aditya shinde Java + Selenium Core 02-Jun-2023 1:57:10 pm
+ */
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
