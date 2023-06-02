e9000e12138feb0c2505b23ebc8874dc08b030b9
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-06-02 14:23:32 -0400

    c1

---
+++ P1/src/github/hellojava.java
@@ -0,0 +1,16 @@
+package github;
+
+/**
+ * aditya shinde Java + Selenium Core 02-Jun-2023 2:22:50 pm
+ */
+public class hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello java");
+		
+	}
+
+}
