bb588f6d055b56827c17bf54c41a85ba409ce0d6
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-07-19 12:48:20 -0400

    FAQ 2

--- Formula1/src/FAQ/P6_CountDigitSum.java
+++ Formula1/src/FAQ/P6_CountDigitSum.java
@@ -4,7 +4,19 @@ public class P6_CountDigitSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		int num = 1234;
+		int sum = 0;
+		
+		while(num>0) {
+			
+		sum = sum  + num%10 ;
+		num = num/10;
+		
+		}
 
+		System.out.println(sum);
 	}
 
 }
