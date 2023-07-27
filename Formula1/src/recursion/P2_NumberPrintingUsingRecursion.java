e7b08259f92c7cc01feb85f7861779f1407228fb
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-07-27 12:36:21 -0400

    Update in fibo and number print (Recursion)

--- Formula1/src/recursion/P2_NumberPrintingUsingRecursion.java
+++ Formula1/src/recursion/P2_NumberPrintingUsingRecursion.java
@@ -1,5 +1,12 @@
 package recursion;
-
+/*
+ * Step 1 Create a function with an Args 
+ * Step 2 Apply base condition as (a==11)& return to make recursion finite 
+ * Step 3 Print the number in console 
+ * Step 4 Call out the function with modification m1(a+1) 
+ * means keep increase the value by 1 digit 
+ * 
+ * */
 public class P2_NumberPrintingUsingRecursion {
 
 	public static void m1(int a) {
--- Formula1/src/recursion/P5_FibbonacciSeries.java
+++ Formula1/src/recursion/P5_FibbonacciSeries.java
@@ -1,9 +1,11 @@
 package recursion;
-/*Algorithm For Fibonacci Series 
- * 
- * 
- * 
- * 
+/*
+ * Algorithm For Fibonacci Series 
+ * Step 1  Set The base Condition n=0 and return to controls
+ * Step 2  Do addition between a and b store inside some ref variable 
+ * Step 3  Print the Output in console 
+ * Step 4  Calling of the Function Itself as transfer old value of b to a and new value of c to b  
+ * Step 5  Keep Decrease number of iteration/terms  by single digit as n-1
  * 
  **/
 
