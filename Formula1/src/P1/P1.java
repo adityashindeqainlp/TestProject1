cec909d4e65c8faa0cc8f481e45801aa739fc6f1
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-07-27 08:46:10 -0400

    Recursion Fibboacci

--- Formula1/src/P1/P1.java
+++ Formula1/src/P1/P1.java
@@ -2,10 +2,34 @@ package P1;
 
 public class P1 {
 
+	public static int m2(int a, int b) {// 3
+
+		int c = a + b;
+
+		return c; // 3
+	}
+
+	public static void m1(int num) {
+
+		int d = m2(2, 1); // 3
+		int d1 = d + 25 + 37;
+
+		System.out.println(d);
+
+	}
+
 	public static void main(String[] args) {
 
+		/* 1-5in console */
 
-		System.out.println("I am a Software Engineer");
+//		System.out.println("1");
+//		System.out.println("2");
+//		System.out.println("3");
+//		System.out.println("4");
+//		System.out.println("5");
+
+		System.out.println(m2(1, 2));
+
 	}
 
 }
--- Formula1/src/recursion/P5_FibbonacciSeries.java
+++ Formula1/src/recursion/P5_FibbonacciSeries.java
@@ -1,5 +1,33 @@
 package recursion;
+/*Algorithm For Fibonacci Series 
+ * 
+ * 
+ * 
+ * 
+ * 
+ **/
 
 public class P5_FibbonacciSeries {
 
+	public static void fibo(int a, int b, int n) {
+
+		if (n == 0) {
+			return;
+		}
+		int c = a + b;
+		System.out.println(c);
+		fibo(b, c, n - 1);
+	}
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 1;
+		int n = 7;
+		System.out.println(a);
+		System.out.println(b);
+
+		fibo(a, b, n - 2);
+
+	}
 }
