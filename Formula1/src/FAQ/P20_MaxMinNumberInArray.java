0def337ff1a62554a1a674a403ffa653122680c7
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-03 08:47:12 -0400

    update1

---
+++ Formula1/src/FAQ/P20_MaxMinNumberInArray.java
@@ -0,0 +1,27 @@
+package FAQ;
+
+public class P20_MaxMinNumberInArray {
+
+	public static void main(String[] args) {
+
+		int[] a = { 40, 46, 56, 2, 34, 7, 78, 96, 321, 41, 656, 15 };
+		int temp = 0;
+
+		for (int i = 0; i < a.length; i++) {
+
+			for (int j = i + 1; j < a.length; j++) {
+
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+
+			}
+		}
+
+		System.out.println("Minimium Value in Array : " + a[0]);
+		System.out.println("Maximum Value in Array : " + a[11]);
+	}
+
+}
