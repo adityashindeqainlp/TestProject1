874bf64ef269f4e2143af934cb434b65e216414a
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-03 09:48:55 -0400

    P9

---
+++ Formula1/src/Pattern/P9.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class P9 {
+
+	public static void main(String[] args) {
+
+		// binary
+
+		int n = 4;
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= i; j++) {
+
+				sum = i + j;
+
+				if (sum % 2 == 0) {
+
+					System.out.print("1");
+				} else {
+
+					System.out.print("0");
+				}
+
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+}
