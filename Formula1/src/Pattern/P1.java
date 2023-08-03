0ae062e94b99b90124cb403046aedc9ce11a1c59
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-03 09:50:40 -0400

    P1

---
+++ Formula1/src/Pattern/P1.java
@@ -0,0 +1,25 @@
+package Pattern;
+
+public class P1 {
+
+	public static void main(String[] args) {
+
+		/* Solid SQ */
+
+		int a = 4;
+		int b = 4;
+
+		for (int i = 1; i <= a; i++) {
+
+			for (int j = 1; j <= b; j++) {
+
+				System.out.print("*");
+
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
