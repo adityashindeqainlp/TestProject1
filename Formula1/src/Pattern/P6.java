4a9ebee13594ac12106097ffdc2fcbd0bc4d85e3
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-03 09:49:33 -0400

    p6

---
+++ Formula1/src/Pattern/P6.java
@@ -0,0 +1,28 @@
+package Pattern;
+
+public class P6 {
+
+	public static void main(String[] args) {
+
+		// inverted flip half pyramid
+
+		int n = 4;
+
+		for (int i = n; i >= 1; i--) {
+
+			for (int j = 1; j <= n - i; j++) {
+
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
