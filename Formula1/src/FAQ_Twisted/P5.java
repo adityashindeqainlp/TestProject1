2e46151547774eeaee6fecfa79d92bd96199869a
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-04 15:02:56 -0400

    P5

---
+++ Formula1/src/FAQ_Twisted/P5.java
@@ -0,0 +1,25 @@
+ package FAQ_Twisted;
+
+/*naN scenario
+ * 
+ * 
+ * 
+ * */
+
+
+public class P5 {
+	
+	
+	public static void main(String[] args) {
+		
+	//System.out.println(2/0);
+	
+	System.out.println(2.0/0.0);
+	
+	System.out.println(0.0/0.0);
+	
+	System.out.println(Math.sqrt(-1));
+		
+	}
+
+}
