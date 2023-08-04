aec0d5dbef550c787cfd97b5c293954c2bc25295
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-04 15:03:14 -0400

    P4

---
+++ Formula1/src/FAQ_Twisted/P4.java
@@ -0,0 +1,24 @@
+package FAQ_Twisted;
+
+/* Integer Comparison (Integer Caching)
+ * Range from -128 to 127 
+ * */
+
+public class P4 {
+
+	public static void main(String[] args) {
+
+		Integer num1 = 190;
+		Integer num2 = 190;
+
+		if (num1 == num2) {
+
+			System.out.println("Integers are matching ");
+		} 
+		else {
+
+			System.out.println("Intgers are not matching");
+		}
+
+	}
+}
