bc12922ac69c1dd0354d4e2007a295d2f95a5db6
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-21 16:16:38 -0400

    P11

---
+++ Formula1/src/FAQ_Twisted/P11.java
@@ -0,0 +1,24 @@
+package FAQ_Twisted;
+
+public class P11 {
+	
+	/** 
+	 * Highest Limit of the args pass through the method 
+	 * ** for all data types if we use non static method limit should be 254 args
+	 * ** for all data types if we use static method limit should be 255 args
+	 * 
+	 **/
+	
+	public void m1(int a , int b , int c ) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+}
